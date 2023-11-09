6d255fe4cff4eb806fc9bf7d15bc485939e12361
DavidBariselli <d.bariselli@invisible.com>
2023-11-09 16:07:59 +0100

    feafea

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,41 @@
 import servizi.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static final String PATH = "C:\\Progetti\\Test\\Files";
     public static final String PATHOUT = "C:\\Progetti\\Test\\Files\\Output";
     public static void main(String[] args) throws IOException {
 
-        System.out.println("");
+        List<String> temp = new ArrayList<>();
+
+        temp.add("12");
+        temp.add("AF");
+        temp.add("GG");
+        temp.add("45");
+        temp.add("");
+        temp.add("GRGR");
+        temp.add("RFDE");
+
+        temp = temp.stream()
+                .map(p -> p.isEmpty() ? "123" : p)
+                .sorted()
+                .collect(Collectors.toList());
+
+        System.out.println(temp);
+
+        Map<String, List<String>> test = new HashMap<>();
 
 
-        Map<Key, List<String>> map =  LeggiFile.findAllFilesInFolder(PATH);
-        ScriviFile.scriviFile(map,PATHOUT);
-        InvocaServizio.invocaServizi(PATHOUT);
+
+//        Map<Key, List<String>> map =  LeggiFile.findAllFilesInFolder(PATH);
+//        ScriviFile.scriviFile(map,PATHOUT);
+//        InvocaServizio.invocaServizi(PATHOUT);
 
     }
 }
