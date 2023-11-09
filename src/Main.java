13f6deb36635e4591f72edef9749484f7d308843
DavidBariselli <d.bariselli@invisible.com>
2023-11-09 15:44:08 +0100

    feafea

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static final String PATHOUT = "C:\\Progetti\\Test\\Files\\Output";
     public static void main(String[] args) throws IOException {
 
-        
+        System.out.println("");
 
 
         Map<Key, List<String>> map =  LeggiFile.findAllFilesInFolder(PATH);
