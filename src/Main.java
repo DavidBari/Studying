041430593076a6918a9cefe61b11aad01872bd05
DavidBariselli <d.bariselli@invisible.com>
2023-11-09 16:27:38 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -11,31 +11,31 @@ public class Main {
     public static final String PATH = "C:\\Progetti\\Test\\Files";
     public static final String PATHOUT = "C:\\Progetti\\Test\\Files\\Output";
     public static void main(String[] args) throws IOException {
+//
+//        List<String> temp = new ArrayList<>();
+//
+//        temp.add("12");
+//        temp.add("AF");
+//        temp.add("GG");
+//        temp.add("45");
+//        temp.add("");
+//        temp.add("GRGR");
+//        temp.add("RFDE");
+//
+//        temp = temp.stream()
+//                .map(p -> p.isEmpty() ? "123" : p)
+//                .sorted()
+//                .collect(Collectors.toList());
+//
+//        System.out.println(temp);
+//
+//
+//
 
-        List<String> temp = new ArrayList<>();
 
-        temp.add("12");
-        temp.add("AF");
-        temp.add("GG");
-        temp.add("45");
-        temp.add("");
-        temp.add("GRGR");
-        temp.add("RFDE");
-
-        temp = temp.stream()
-                .map(p -> p.isEmpty() ? "123" : p)
-                .sorted()
-                .collect(Collectors.toList());
-
-        System.out.println(temp);
-
-        Map<String, List<String>> test = new HashMap<>();
-
-
-
-//        Map<Key, List<String>> map =  LeggiFile.findAllFilesInFolder(PATH);
-//        ScriviFile.scriviFile(map,PATHOUT);
-//        InvocaServizio.invocaServizi(PATHOUT);
+        Map<Key, List<String>> map =  LeggiFile.findAllFilesInFolder(PATH);
+        ScriviFile.scriviFile(map,PATHOUT);
+        InvocaServizio.invocaServizi(PATHOUT);
 
     }
 }
